369a604c88207d4f8818b394d6af0fd67bbf9e8e
MEGA <youssefabdelmaged50@gmail.com>
2026-09-17 17:20:50 +0300

    feat: add Menu and Contact components with GSAP animations and App setup

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import gsap from 'gsap'
 import { Cocktails } from './components/Cocktails'
 import About from './components/About'
 import Art from './components/Art'
+import Menu from './components/Menu'
+import Contact from './components/Contact'
 gsap.registerPlugin(ScrollTrigger,SplitText)
 
 const App = () => {
@@ -16,6 +18,8 @@ const App = () => {
     <Cocktails/>
     <About/>
     <Art/>
+    <Menu/>
+    <Contact/>
     </main>
   )
 }
